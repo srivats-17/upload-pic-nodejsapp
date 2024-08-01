824f908070ede6347d361389f39eeda21441bed4
jamalla somasekhar <somasekharjamalla@gmail.com>
2024-08-01 10:59:20 +0530

    awsebs add file dockerrun.aws.jso

---
+++ scrpts/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Install dependencies
+cd /home/ec2-user/nodejs-app
+npm install
---
+++ scrpts/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Start the Node.js application
+cd /home/ec2-user/nodejs-app
+pm2 start index.js
